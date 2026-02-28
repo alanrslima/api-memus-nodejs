9bb67623c62e370afbcd477cd04b505a9b20f6c2
Alan Lima <alanronison.lima@gmail.com>
2026-02-28 18:35:42 -0300

    fix: removed instrument file

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -2,19 +2,16 @@ import "express-async-errors";
 import "../infra/observability/sentry";
 import Sentry from "@sentry/node";
 
-import https from "https";
 import http from "http";
 import express from "express";
 
 import { setupMiddleware } from "./middleware";
 import { errorHandler } from "../middleware/error-handler";
-import fs from "node:fs";
 
 import { authRouter } from "../../module/auth";
 import { memoryRouter } from "../../module/memory";
 import { geolocationRouter } from "../../module/geolocation";
 import { setupEventListener } from "./event-listener";
-import { env } from "../../module/common";
 
 const app = express();
 
